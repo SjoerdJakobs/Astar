572469a5efd1beb1dcefe2720caa0f54a1ab48bc
SjoerdJakobs <18284@ma-web.nl>
2016-09-19 23:27:39 +0200

    the start of my own A*
    here i will try to make my own a*

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Node))]
+public class Grid : MonoBehaviour {
+    private Node node;
+
+    public Node[] _open;
+    public Node[] _closed;
+
+
+    private int xSize;
+    private int ySize;
+	// Use this for initialization
+	void Start () {
+	}
+
+    void CreateGrid()
+    {
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < xSize; y++)
+            {
+
+            }
+        }
+    }
+    	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node
+{
+    public Vector3 _position;
+    public bool _walkable;
+    public float _gCost;
+    public float _hCost;
+    public float _fCost;
+
+    public Node(Vector3 position, bool walkable, float gCost, float hCost)
+    {
+        _position = position;
+        _walkable = walkable;
+        _gCost = gCost;
+        _hCost = hCost;
+    }
+}
