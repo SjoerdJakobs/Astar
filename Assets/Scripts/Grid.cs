6884d1f2f335759e3b2d04681e2e0003303af624
SjoerdJakobs <18284@ma-web.nl>
2016-09-21 12:44:20 +0200

    sync 2
    sync 2

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,19 +4,50 @@ using System.Collections;
 
 public class Grid : MonoBehaviour {
 
-    public Node[,] grid;
-
+    //Bool
     [SerializeField]
-    private int xSize;
+    private bool showGrid;//do you want to see the gizmo's in the editor?
     [SerializeField]
-    private int ySize;
+    private bool dynamicGrid;//do you want to update the grid for changes
+    //Bool
 
-	// Use this for initialization
-	void Start () {
+    //LayerMasks
+    [SerializeField]
+    private LayerMask unwalkableMask;//the layermask that the obstacles will have where you cant walk.
+                                     //LayerMasls
+
+    //Vector2
+    [SerializeField]
+    private Vector2 gridSize;//a vector 2 that decides the size of the grid
+    //Vector2
+
+    //Float
+    [SerializeField]
+    private float enemyRadius;
+    [SerializeField]
+    private float refreshRate;
+    [SerializeField]
+    private float nodeRadius;//the radius of the node
+    private float nodeDiameter;//the diameter of the nodes
+    //Float
+
+    //Scripts
+    private Node[,] grid;//a 2d array of nodes that will make the grid
+                          //Scripts
+
+
+    //Int
+    private int gridSizeX, gridSizeY;//the amount of nodes that wil be in a x line and a y line in the grid(magic trick: do x*y and you get all the nodes in the grid! :D)
+                                       //Int
+
+    // Use this for initialization
+    void Awake () {
+        CreateGrid();
 	}
 
     void CreateGrid()
     {
+        /*grid = new Node[xSize, ySize];
         for (int x = 0; x < xSize; x++)
         {
             for (int y = 0; y < xSize; y++)
@@ -25,11 +56,23 @@ public class Grid : MonoBehaviour {
                 
                 grid[x, y] = node;
             }
-        }
+        }*/
     }
     	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnDrawGizmos()
+    {
+        /*for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < xSize; y++)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawCube(grid[x, y]._position, new Vector3(0.5f, 0.5f,0.5f));
+            }
+        }*/
+    }
 }
